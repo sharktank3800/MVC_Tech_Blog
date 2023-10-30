c6fe46b60b8b21ce18a41554e5537dd8597d792e
[Ayman Shiham] <sharktank38000@gmail.com>
2023-10-30 18:06:39 -0400

    edited post.js in models, replaced columns and implemented dayjs

--- models/Post.js
+++ models/Post.js
@@ -1,25 +1,31 @@
 const { Model, DataTypes } = require("sequelize");
 const db = require("../config/connection");
-
 const dayjs = require("dayjs");
 
 
 class Post extends Model { }
 
 Post.init({
-  text: {
+  title: {
     type: DataTypes.STRING,
+    allowNull: false,
+  },
+
+  content: {
+    type: DataTypes.STRING,
+    allowNull: false,
     validate: {
       len: {
-        args: 3,
-        msg: "Your coo message must be at least 3 characters in length."
+        args: 4,
+        msg: "Post content must be at least 4 characters in length."
       }
     }
   },
+
   date: {
     type: DataTypes.VIRTUAL,
-    get() {
-      return dayjs(this.createdAt).format("MM/DD/YYYY hh:mm")
+    get(){
+      return dayjs(this.createdAt).format("MM/DD/YYYY")
     }
   }
 }, {
