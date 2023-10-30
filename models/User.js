5dc6422e52719898527101b0a9b6d4ecacee7675
[Ayman Shiham] <sharktank38000@gmail.com>
2023-10-30 17:55:42 -0400

    added hook function beforecreate taking user obj for hasing users pass before it is saved  and function to validate pass

--- models/User.js
+++ models/User.js
@@ -1,37 +1,48 @@
-const {Model, DataTypes} = require("sequelize");
-const db = require("../config/connection")
-const {hash, compare} = require("bcrypt");
+const { Model, DataTypes } = require("sequelize");
+const db = require("../config/connection");
+const { hash, compare } = require("bcrypt");
 
+class User extends Model {}
 
-class User extends Model{ }
-
-
-User.init({
-    username:{
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: {
-            args: true,
-            msg: "Username is already in use."
-        }
+User.init(
+  {
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: {
+        args: true,
+        msg: "Username is already in use.",
+      },
     },
 
     password: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-            len: {
-                args: 6,
-                msg: "Password must be at least 6 Characters long."
-            }
-        }
-    }
-}, {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: {
+          args: 6,
+          msg: "Password must be at least 6 Characters long.",
+        },
+      },
+    },
+  },
+  {
     modelName: "user",
     // connection object
-    sequelize: db
-})
+    sequelize: db,
+    hooks: {
+      async beforeCreate(user) {
+        user.password = await hash(user.password, 10);
+        return user;
+      },
+    },
+  }
+);
 
+User.prototype.validatePass = async function(formPassword){
+    const isValid = await compare(formPassword, this.password);
+    return isValid;
+}
 
 
 module.exports = User;
